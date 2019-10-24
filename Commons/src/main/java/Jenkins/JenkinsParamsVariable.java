5b713cf480e6c5ca5e8c2b6539e05ceb9dd4b3ab
Vinit Sandesara <vinit.sandesara@footlocker.com>
2019-10-24 14:59:34 -0500

    passing value from jenkins

--- Commons/src/main/java/Jenkins/JenkinsParamsVariable.java
+++ Commons/src/main/java/Jenkins/JenkinsParamsVariable.java
@@ -3,11 +3,11 @@ package Jenkins;
 public class JenkinsParamsVariable {
 
     /* Jenkins run */
-   // public static String GoogleDriveSpreadSheetName = System.getProperty("SpreadSheetName");
-   // public static String GoogleDriveSpreadSheetRowNumber = System.getProperty("SheetRowNumber");
+    public static String GoogleDriveSpreadSheetName = System.getProperty("SpreadSheetName");
+    public static String GoogleDriveSpreadSheetRowNumber = System.getProperty("SheetRowNumber");
 
-    /* Local Run */
+    /* Local Run
      public static String GoogleDriveSpreadSheetName = "NewUser_FL_KFL_FA_CS";
-    public static String GoogleDriveSpreadSheetRowNumber = "2";
+    public static String GoogleDriveSpreadSheetRowNumber = "2"; */
 
 }
